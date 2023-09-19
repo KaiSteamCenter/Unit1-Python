a12652c70884c5fa1707588ca5850354414d699e
Mekhi M <mmcfowler@brooklynsteamcenter.org>
2023-09-19 09:50:33 -0400

    hi

---
+++ benchmark.py
@@ -0,0 +1,40 @@
+# Excercise 1: Display name in console
+
+print('Mekhi Mcfowler') 
+
+# Excercise 2: Create Variable called favorite show
+
+favshow = 'The Blacklist'
+
+# Excercise 3: Create 2 variables named height and age
+
+height = 170.18
+age = 17
+
+print(height*age)
+
+# Excercise 4:
+print('Im going to break the console')
+
+# Excercise 5: Create list named fav_food
+fav_Food = ["Chicken", "Watermelon", "Pizza"]
+print(fav_Food)
+
+# Excercise 6: 
+if age >= 21:
+    print("You can rent a car")
+else:
+    print("your beat")
+
+# Excercise 7:
+
+name = input("What is your name: ")
+print("Hello "+ name)
+
+# Excercise 8:
+for x in fav_Food:
+    print(x)
+
+# Excercise 9:
+while age < 16:
+    print("This won't ever run")
