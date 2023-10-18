dc1676887fad7b3b455912376a43a960584246e0
Mekhi M <mmcfowler@brooklynsteamcenter.org>
2023-10-18 10:43:31 -0400

    twirlanta

--- returnfunction.py
+++ returnfunction.py
@@ -7,6 +7,16 @@ def squareNum(a): # Creates function
     return a**2 # Returns value and does the power to the second.
 print(squareNum(10)) # Calls function
 
+assert squareNum(7) == 49
+try:
+    assert squareNum(5) == 25
+except:
+    print("Assert is wrong.")
+
+# Assert the result of calling 'squareNum' with the argument: 7
+# Assert the result of calling 'squareNum' with the argument: 5
+# If the assertion in the 'try/except' block fails, print "Assert is wrong."
+
 
 """
 Task 2: Calculate the Area of a Rectangle:
@@ -16,6 +26,16 @@ Write a function that takes the length and width of a rectangle and returns its 
 def rectArea(a, b): # Creates function
     return a * b # Returns value and multiples a by b
 print(rectArea(5, 10)) # Calls function
+
+assert rectArea(10,10) == 100
+try:
+    assert rectArea(2.5, 5) == 12.5
+except:
+    print("Assert is wrong.")
+
+# Assert the result of calling 'rectArea' with the 2 arguments: 10 , 10
+# Assert the result of calling 'rectArea' with the 2 arguments: 2.5, 5
+# If the assertion in the 'try/except' block fails, print "Assert is wrong."
 
 """
 Task 3: Convert Temperature from Celsius to Fahrenheit:
@@ -27,6 +47,15 @@ def temp(a): # Creates function
     return (a * (9/5)) + 32 # returns value, multiples a by (9/5), and adds 32
 print(temp(1),"°C") # Calls function
 
+assert temp(5) == 41
+try:
+    assert temp(18) == 64.4
+except:
+    print("Assert is wrong.")
+
+# Assert the result of calling 'temp' with the argument: 5
+# Assert the result of calling 'avgNumber' with the argument: 18
+# If the assertion in the 'try/except' block fails, print "Assert is wrong."
 """
 Task 4: Calculate the Average of Numbers:
 Write a function that takes a list of numbers 
@@ -38,3 +67,12 @@ def avgNumber(a): # Creates function
     return total / len(a) # Returns and retieves avg by dividing total by length of avg
 
 print(avgNumber([6, 5, 4, 6, 2, 9, 15])) # Calls function
+
+assert avgNumber([6, 4, 32, 5, 4, 2, 3]) == 8 
+try:
+    assert avgNumber([5,10,15]) == 10
+except:
+    print("Assert is wrong.")
+# Assert the result of calling 'avgNumber' with the first list
+# Assert the result of calling 'avgNumber' with the second list
+# If the assertion in the 'try/except' block fails, print "Assert is wrong."
